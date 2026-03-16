e1c2eb7cee42c3ca5083b17e34d1364e328831ce
Olvin Villalobos <olvinvillalobos25@gmail.com>
2026-03-16 14:03:49 +0000

    he terminado el ejercicio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function(){
+    let who = ['El Perro', 'Mi Abuela', 'El Cartero', 'Mi Pajaro'];
+    let action = ['comió', 'meó en', 'aplastó', 'se rompió'];
+    let what = ['mi tarea', 'mi teléfono', 'el coche'];
+    let when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras hacia ejercicio', 'durante mi almuerzo', 'mientras estaba rezando'];
+    function generateExcuse(){
+    let whoRandom = who[Math.floor(Math.random() * who.length)];
+    let actionRandom = action[Math.floor(Math.random() * action.length)];
+    let whatRandom = what[Math.floor(Math.random() * what.length)];
+    let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+    return whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+    }
+
+    this.document.querySelector("#excuse").innerHTML = generateExcuse();
+
 };
